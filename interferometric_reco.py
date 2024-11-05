61d6b7cf4e1426220366c603f54b032775b677fb
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2024-11-04 22:04:03 -0600

    add xy maps

--- interferometric_reco.py
+++ interferometric_reco.py
@@ -39,35 +39,42 @@ def worker_rz(wargs):
     with open(outpath, 'wb') as outfile:
         pickle.dump(reco, outfile)
 
-# def worker_xy(wargs):
-#     eventpath = wargs["eventpath"]
-#     outdir = wargs["outdir"]
-#     mappath = wargs["mappath"]
-#     res = wargs["resolution"]
+def worker_xy(wargs):
+    eventpath = wargs["eventpath"]
+    outdir = wargs["outdir"]
+    mappath = wargs["mappath"]
+    res = wargs["resolution"]
+    det = wargs["detector"]
 
-#     with open(eventpath, 'rb') as eventfile:
-#         event = pickle.load(eventfile)
+    with open(eventpath, 'rb') as eventfile:
+        event = pickle.load(eventfile)
+        channel_signals = event["signals"]
+        channel_times = event["times"]
 
-#     basename = os.path.splitext(os.path.basename(eventpath))[0]
+    basename = os.path.splitext(os.path.basename(eventpath))[0]
     
-#     outpath = os.path.join(outdir, f"{basename}_xy.pkl")
-#     print(f"Reconstructing {eventpath} -> {outpath}")
+    outpath = os.path.join(outdir, f"{basename}_xy.pkl")
+    print(f"Reconstructing {eventpath} -> {outpath}")
     
-#     x_range = (-900, 900)
-#     y_range = (-900, 900)
+    x_range = (-300, 300)
+    y_range = (-300, 300)
 
-#     z_slice = -1 / defs.cvac
+    z_slice = -80 / defs.cvac
     
-#     coord_start = [x_range[0], y_range[0], z_slice]
-#     coord_end =   [x_range[1], y_range[1], z_slice]
+    coord_start = [x_range[0], y_range[0], z_slice]
+    coord_end =   [x_range[1], y_range[1], z_slice]
     
-#     coinc_window_size = 10    #Size in bins to look for peak-peak voltage
-#     # channels_to_include = utils.get_high_SNR_channels(event, coinc_window_size, remove_edge_signals=True)
-#     channels_to_include = [0, 1, 2, 3, 5, 6, 7, 9, 10, 22, 23]
+    channels_to_include = [0, 1, 2, 3, 22, 23]
+    channel_positions = det.get_channel_positions(station_id = 11, channels = channels_to_include)
+    cable_delays = det.get_cable_delays(station_id = 11, channels = channels_to_include)
  
-#     reco_utils.interferometric_reco(event, outpath, mappath, coord_start = coord_start, coord_end = coord_end,
-#                                     num_pts = [res, res, 1],
-#                                     channels_to_include=channels_to_include)
+    reco = reco_utils.interferometric_reco(channel_signals, channel_times, outpath, mappath,
+                                           coord_start = coord_start, coord_end = coord_end, num_pts = [res, res, 1],
+                                           channels_to_include = channels_to_include,
+                                           channel_positions = channel_positions, cable_delays = cable_delays)
+    
+    with open(outpath, 'wb') as outfile:
+        pickle.dump(reco, outfile)
         
 if __name__ == "__main__":
 
@@ -99,6 +106,6 @@ if __name__ == "__main__":
         with Pool(num_workers) as p:
             p.map(worker_rz, wargs)
 
-    # if args.do_xy:
-    #     with Pool(num_workers) as p:
-    #         p.map(worker_xy, wargs)
+    if args.do_xy:
+        with Pool(num_workers) as p:
+            p.map(worker_xy, wargs)
