833123b3b2c6d72d80f684a2d333c69464ba060e
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2024-11-06 10:48:47 -0600

    shows how to plot map

---
+++ plotting_utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+import defs
+
+def show_map(ax, intmap, axis_a, axis_b, cmap = "bwr", aspect = "auto", vmin = None, vmax = None, fs = 13):
+
+    intmap_to_plot = np.squeeze(intmap["map"])
+    
+    if vmin is None:
+        vmin = np.min(intmap_to_plot)
+
+    if vmax is None:
+        vmax = np.max(intmap_to_plot)
+
+    cscale = max(abs(vmin), abs(vmax))
+    vmin, vmax = -cscale, cscale
+
+    im = ax.imshow(np.flip(np.transpose(intmap_to_plot), axis = 0),
+                   extent = [intmap[axis_a][0] * defs.cvac, intmap[axis_a][-1] * defs.cvac,
+                             intmap[axis_b][0] * defs.cvac, intmap[axis_b][-1] * defs.cvac],
+                   cmap = cmap, vmax = vmax, vmin = vmin, aspect = aspect)
+    
+    ax.set_xlabel(f"{axis_a} [m]", fontsize = fs)
+    ax.set_ylabel(f"{axis_b} [m]", fontsize = fs)
+    ax.tick_params(axis = "y", direction = "in", left = True, right = True, labelsize = fs)
+    ax.tick_params(axis = "x", direction = "in", bottom = True, top = True, labelsize = fs)
+
+    return im
